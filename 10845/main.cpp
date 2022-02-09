bad145fe7c9083b97961a740547727c70f09132d
bmkwak <bmkwak22@gmail.com>
2022-02-09 23:46:50 +0900

    알고리즘 기초 1/2 200 - 자료구조 1 - 큐 10845 큐 https://www.acmicpc.net/problem/10845

---
+++ 10845/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+int main()
+{	
+	int remainCommandNumber;
+	string command;
+	int number;
+	queue<int> q;
+
+	// 명령어 수 입력
+	cin >> remainCommandNumber;
+
+	while (remainCommandNumber--)
+	{
+		cin >> command;
+
+		if (command.compare("push") == 0)
+		{
+			cin >> number;
+			q.push(number);
+		}
+		else if (command.compare("pop") == 0)
+		{
+			if (q.empty())
+			{
+				cout << -1 << endl;
+			}
+			else
+			{
+				cout << q.front() << endl;
+				q.pop();
+			}
+		}
+		else if (command.compare("size") == 0)
+		{
+			cout << q.size() << endl;
+		}
+		else if (command.compare("empty") == 0)
+		{
+			cout << q.empty() << endl;
+		}
+		else if (command.compare("front") == 0)
+		{
+			if (q.empty())
+			{
+				cout << -1 << endl;
+			}
+			else
+			{
+				cout << q.front() << endl;
+			}
+		}
+		else if (command.compare("back") == 0)
+		{
+			if (q.empty())
+			{
+				cout << -1 << endl;
+			}
+			else
+			{
+				cout << q.back() << endl;
+			}
+		}
+	}
+
+	return 0;
+}
