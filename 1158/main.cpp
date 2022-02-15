8d03437edb139e92ea5e340d80720bcbfe5827b1
bmkwak <bmkwak22@gmail.com>
2022-02-15 23:00:15 +0900

    알고리즘 기초 1/2 200 - 자료구조 1 - 요세푸스 문제 1158 요세푸스 문제 https://www.acmicpc.net/problem/1158

---
+++ 1158/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{	
+	int n, k;
+	int i;
+	queue<int> q;
+
+	cin >> n;
+	cin >> k;
+
+	for (i = 1; i <= n; i++)
+	{
+		q.push(i);
+	}
+
+	cout << "<";
+
+	while (1)
+	{
+		for (i = 1; i < k; i++)
+		{
+			q.push(q.front());
+			q.pop();
+		}
+
+		cout << q.front();
+		q.pop();
+
+		if (q.empty())
+		{
+			break;
+		}
+		else
+		{
+			cout << ", ";
+		}
+	}
+
+	cout << ">";
+
+	return 0;
+}
