edf74331fa4f0d20a1792d0decd2f048078b93ca
bmkwak <bmkwak22@gmail.com>
2022-02-18 22:25:40 +0900

    알고리즘 기초 1/2 201 - 자료구조 1 (연습) - 단어 뒤집기 2 17413 단어 뒤집기 2 https://www.acmicpc.net/problem/17413

---
+++ 17413/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+stack<char> s;
+void PrintStack();
+
+int main()
+{	
+	string str;
+	int strLen;
+	int i;
+
+	getline(cin, str);
+	strLen = str.length();
+	
+	for (i = 0; i < strLen; i++)
+	{
+		if (str[i] == '<')
+		{
+			PrintStack();
+
+			for (i = i; i < strLen; i++)
+			{
+				cout << str[i];
+				if (str[i] == '>')
+				{
+					break;
+				}
+			}
+		}
+		else if(str[i] == ' ')
+		{
+			PrintStack();
+			cout << ' ';
+		}
+		else
+		{
+			s.push(str[i]);
+		}
+	}
+
+	PrintStack();
+
+	return 0;
+}
+
+void PrintStack()
+{
+	while (!s.empty())
+	{
+		cout << s.top();
+		s.pop();
+	}
+}
