19e8d46eeaca5201ded421412bacb80044aefcf7
bmkwak <bmkwak22@gmail.com>
2022-02-22 21:35:47 +0900

    알고리즘 기초 1/2 201 - 자료구조 1 (연습) - 쇠막대기 10799 쇠막대기 https://www.acmicpc.net/problem/10799

---
+++ 10799/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{	
+	int result;				// ÃÑ Á¶°¢ ¼ö
+	int ironStickNumber;	// ½×¿©ÀÖ´Â ¼è¸·´ë±â ¼ö
+	string input;
+
+	result = 0;
+	ironStickNumber = 0;
+
+	cin >> input;
+
+	for (int i = 0; i < input.length(); i++)
+	{
+		if (input[i] == '(')
+		{
+			ironStickNumber++;
+		}
+		else
+		{
+			ironStickNumber--;
+
+			if (input[i - 1] == '(')
+			{
+				result += ironStickNumber;
+			}
+			else
+			{
+				result++;
+			}
+		}
+	}
+
+	cout << result << endl;
+
+	return 0;
+}
