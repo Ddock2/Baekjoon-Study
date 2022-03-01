8b855fce0c85280b0abb54dbaa0db6e147fba675
bmkwak <bmkwak22@gmail.com>
2022-03-01 21:22:08 +0900

    알고리즘 기초 1/2 203 - 자료구조 1 (참고) - 후위 표기식2 1935 후위 표기식2 https://www.acmicpc.net/problem/1935

---
+++ 1935/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main()
+{	
+	int numberList[26];
+	int number;
+	string line;
+	string convertLine;
+	int i;
+
+	stack<double> s;
+	double tmp;
+
+	// input
+	cin >> number;
+	cin >> line;
+	for (i = 0; i < number; i++)
+	{
+		cin >> numberList[i];
+	}
+
+	// °è»ê
+	for (i = 0; i < line.length(); i++)
+	{
+		if ('A' <= line[i] && line[i] <= 'Z')
+		{
+			s.push(numberList[line[i] - 'A']);
+		}
+		else
+		{
+			tmp = s.top();
+			s.pop();
+
+			switch (line[i])
+			{
+				case '+':
+					tmp = s.top() + tmp;
+					break;
+				case '-':
+					tmp = s.top() - tmp;
+					break;
+				case '*':
+					tmp = s.top() * tmp;
+					break;
+				case '/':
+					tmp = s.top() / tmp;
+					break;
+			}
+
+			s.pop();
+			s.push(tmp);
+		}
+	}
+
+	cout << fixed;
+	cout.precision(2);
+	cout << s.top() << endl;
+
+	return 0;
+}
