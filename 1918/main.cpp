f4ff249f68fa3beb2a6ecfec9a3b633eeb3db15a
bmkwak <bmkwak22@gmail.com>
2022-03-02 22:12:22 +0900

    알고리즘 기초 1/2 203 - 자료구조 1 (참고) - 후위 표기식 1918 후위 표기식 https://www.acmicpc.net/problem/1918

---
+++ 1918/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main()
+{	
+	string input;
+	stack<char> tmp;
+	int i;
+
+	cin >> input;
+	
+	for (i = 0; i < input.length(); i++)
+	{
+		if ('A' <= input[i] && input[i] <= 'Z')
+		{
+			cout << input[i];
+		}
+		else
+		{
+			if (input[i] == '(')
+			{
+				tmp.push(input[i]);
+			}
+			else if (input[i] == ')')
+			{
+				while (!tmp.empty() && tmp.top() != '(')
+				{
+					cout << tmp.top();
+					tmp.pop();
+				}
+				tmp.pop();
+			}
+			else if (input[i] == '+' || input[i] == '-')
+			{
+				while (!tmp.empty() && tmp.top() != '(')
+				{
+					cout << tmp.top();
+					tmp.pop();
+				}
+				tmp.push(input[i]);
+			}
+			else if (input[i] == '*' || input[i] == '/')
+			{
+				while (!tmp.empty() && (tmp.top() == '*' || tmp.top() == '/'))
+				{
+					cout << tmp.top();
+					tmp.pop();
+				}
+				tmp.push(input[i]);
+			}
+		}
+	}
+
+	while (!tmp.empty())
+	{
+		cout << tmp.top();
+		tmp.pop();
+	}
+
+	return 0;
+}
