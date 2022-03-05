b57cd249cefda915096e533d07a179b2f3a3ef27
bmkwak <bmkwak22@gmail.com>
2022-03-06 00:25:47 +0900

    알고리즘 기초 1/2 203 - 자료구조 1 (참고) - 알파벳 개수 10808 알파벳 개수 https://www.acmicpc.net/problem/10808

---
+++ 10808/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{	
+	string input;
+
+	int charNumList['z' - 'a' + 1] = {0};
+	int i;
+	
+	cin >> input;
+
+	for (i = 0; i < input.length(); i++)
+	{
+		charNumList[input[i] - 'a']++;
+	}
+
+	for (i = 0; i <= 'z' - 'a'; i++)
+	{
+		cout << charNumList[i] << ' ';
+	}
+
+	return 0;
+}
