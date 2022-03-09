01ff8267935105ba03a84d975385e4ffdbf82a8f
bmkwak <bmkwak22@gmail.com>
2022-03-10 01:03:26 +0900

    알고리즘 기초 1/2 203 - 자료구조 1 (참고) - 문자열 분석 10820 문자열 분석 https://www.acmicpc.net/problem/10820

---
+++ 10820/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{	
+	string input;
+
+	int lowerNumber;
+	int upperNumber;
+	int numberNumber;
+	int spaceNumber;
+	int i;
+
+	while (getline(cin, input))
+	{
+		lowerNumber = 0;
+		upperNumber = 0;
+		numberNumber = 0;
+		spaceNumber = 0;
+
+		for (i = 0; i < input.length(); i++)
+		{
+			if ('a' <= input[i] && input[i] <= 'z')
+			{
+				lowerNumber++;
+			}
+			else if ('A' <= input[i] && input[i] <= 'Z')
+			{
+				upperNumber++;
+			}
+			else if ('0' <= input[i] && input[i] <= '9')
+			{
+				numberNumber++;
+			}
+			else if (' ' == input[i])
+			{
+				spaceNumber++;
+			}
+		}
+
+		cout << lowerNumber << ' ' << upperNumber << ' ' << numberNumber << ' ' << spaceNumber << endl;
+	}
+
+	return 0;
+}
