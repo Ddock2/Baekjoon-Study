e0dff5af76c4208b0e0ad6e177207be24b601bc4
bmkwak <bmkwak22@gmail.com>
2022-03-10 00:04:25 +0900

    알고리즘 기초 1/2 203 - 자료구조 1 (참고) - 알파벳 찾기 10809 알파벳 찾기 https://www.acmicpc.net/problem/10809

---
+++ 10809/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{	
+	string input;
+
+	int charNumList['z' - 'a' + 1] = {-1, };
+	int i;
+	
+	cin >> input;
+
+	for (i = 0; i <= 'z' - 'a'; i++)
+	{
+		charNumList[i] = -1;
+	}
+
+	for (i = 0; i < input.length(); i++)
+	{
+		if (charNumList[input[i] - 'a'] == -1)
+		{
+			charNumList[input[i] - 'a'] = i;
+		}
+	}
+
+	for (i = 0; i <= 'z' - 'a'; i++)
+	{
+		cout << charNumList[i] << ' ';
+	}
+
+	return 0;
+}
