49581628d4dae76bc0e53f164084b3f45fce783f
bmkwak <bmkwak22@gmail.com>
2022-03-17 22:24:09 +0900

    알고리즘 기초 1/2 203 - 자료구조 1 (참고) - 네 수 10824 네 수 https://www.acmicpc.net/problem/10824

---
+++ 10824/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{	
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+
+	string tmpStr1;
+	string tmpStr2;
+	long long result;
+
+	cin >> num1;
+	cin >> num2;
+	cin >> num3;
+	cin >> num4;
+
+	tmpStr1 = to_string(num1) + to_string(num2);
+	tmpStr2 = to_string(num3) + to_string(num4);
+
+	result = stoll(tmpStr1) + stoll(tmpStr2);
+
+	cout << result << endl;
+
+	return 0;
+}
