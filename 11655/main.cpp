ec8a1d895e344351a645bc8a7ea7e07ebfe0ee31
bmkwak <bmkwak22@gmail.com>
2022-03-17 21:54:06 +0900

    알고리즘 기초 1/2 203 - 자료구조 1 (참고) - ROT13 11655 ROT13 https://www.acmicpc.net/problem/11655

---
+++ 11655/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{	
+	string input;
+	int plus;
+	int i;
+	
+	getline(cin, input);
+
+	for (i = 0; i < input.length(); i++)
+	{
+		if (input[i] >= 'a' && input[i] <= 'z')
+		{
+			plus = (input[i] - 'a' + 13) % 26;
+			cout << (char)('a' + plus);
+		}
+		else if (input[i] >= 'A' && input[i] <= 'Z')
+		{
+			plus = (input[i] - 'A' + 13) % 26;
+			cout << (char)('A' + plus);
+		}
+		else
+		{
+			cout << input[i];
+		}
+	}
+
+	return 0;
+}
