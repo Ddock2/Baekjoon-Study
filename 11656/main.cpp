d3c9d3ad321f447ad6ee19b18c3e7cc11df20dc5
bmkwak <bmkwak22@gmail.com>
2022-03-20 00:16:37 +0900

    알고리즘 기초 1/2 203 - 자료구조 1 (참고) - 접미사 배열 11656 접미사 배열 https://www.acmicpc.net/problem/11656

---
+++ 11656/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{	
+	string input;
+	int inputLenght;
+	string tmp[1000];
+	int i;
+
+	cin >> input;
+	inputLenght = input.length();
+
+	for (i = 0; i < inputLenght; i++)
+	{
+		tmp[i] = input.substr(i, inputLenght - i);
+	}
+
+	sort(tmp, tmp + inputLenght);
+
+	for (i = 0; i < inputLenght; i++)
+	{
+		cout << tmp[i] << endl;
+	}
+
+	return 0;
+}
