c8145da7a88d961bd6ac7cbce348d7425826c742
bmkwak <bmkwak22@gmail.com>
2022-03-25 00:12:36 +0900

    알고리즘 기초 1/2 300 - 수학 1 - 최소공배수 1934 최소공배수 https://www.acmicpc.net/problem/1934

---
+++ 1934/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int CalcGCD(int a, int b);
+int CalcLCM(int a, int b);
+
+int main()
+{	
+	int number;
+	int num1, num2;
+	int tmp;
+
+	cin >> number;
+
+	while (number--)
+	{
+		cin >> num1 >> num2;
+
+		cout << CalcLCM(num1, num2) << endl;
+	}
+
+	return 0;
+}
+
+int CalcGCD(int a, int b)
+{
+	return b ? CalcGCD(b, a % b) : a;
+}
+
+int CalcLCM(int a, int b)
+{
+	return a * b / CalcGCD(a, b);
+}
