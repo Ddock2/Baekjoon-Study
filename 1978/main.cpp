fb87823082c150747a527a9b734c503ae73324f6
bmkwak <bmkwak22@gmail.com>
2022-03-24 23:18:18 +0900

    알고리즘 기초 1/2 300 - 수학 1 - 소수 찾기 1978 소수 찾기 https://www.acmicpc.net/problem/1978

---
+++ 1978/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+bool IsPrimeNumber(int n);
+
+int main()
+{	
+	int number;
+	int tmp;
+	int result;
+
+	result = 0;
+
+	cin >> number;
+
+	while (number--)
+	{
+		cin >> tmp;
+
+		if (IsPrimeNumber(tmp))
+		{
+			result++;
+		}
+	}
+
+	cout << result << endl;
+
+	return 0;
+}
+
+bool IsPrimeNumber(int n)
+{
+	int tmp;
+
+	if (n < 2)
+	{
+		return false;
+	}
+
+	tmp = n - 1;
+
+	while (tmp > 1)
+	{
+		if (n % tmp-- == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
